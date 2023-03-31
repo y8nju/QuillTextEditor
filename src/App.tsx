d735e570f7066989da280bdd7a5b5a37260b417b
y8nju <89aruc@gmail.com>
2023-03-31 11:11:30 +0900

    Add: toolbars, formats utill 분리

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import ReactQull from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './App.css';
+import { Formats, Toolbar } from './utils/index';
 
 function App() {
   const [value, setValue] = useState('');
@@ -9,43 +10,9 @@ function App() {
     <div>
       <ReactQull
         modules={{
-          toolbar: [
-            ['image'],
-            [{ font: [] }, { header: [1, 2, 3, 4, 5, 6, false] }],
-            ['bold', 'italic', 'underline', 'strike', 'code-block', 'link'],
-            [{ color: [] }, { background: [] }, { align: [] }],
-            [
-              { list: 'ordered' },
-              { list: 'bullet' },
-              'blockquote',
-              { indent: '+1' },
-              { indent: '-1' },
-            ],
-            [{ script: 'sub' }, { script: 'super' }],
-            ['clean'],
-          ],
+          toolbar: Toolbar,
         }}
-        formats={[
-          'header',
-          'bold',
-          'italic',
-          'underline',
-          'strike',
-          'blockquote',
-          'code-block',
-          'list',
-          'ordered',
-          'bullet',
-          'script',
-          'indent',
-          'link',
-          'image',
-          'color',
-          'background',
-          'font',
-          'align',
-          'clean',
-        ]}
+        formats={Formats}
         theme="snow"
         value={value}
         onChange={setValue}
